f69dbce7f1d195e0ee92a213dceb85adfb52ef57
devluansouza <luan.souza2014@gmail.com>
2022-08-16 19:54:09 -0300

    :tada: Initial commit
    Uploading project to the repository

---
+++ src/main/java/dev/luansouza/brazilianfootballapi/BrazilianFootballApiApplication.java
@@ -0,0 +1,13 @@
+package dev.luansouza.brazilianfootballapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianFootballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianFootballApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/luansouza/brazilianfootballapi/BrazilianFootballApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luansouza.brazilianfootballapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianFootballApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
